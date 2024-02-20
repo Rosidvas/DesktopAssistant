b04a7d2dc72179e36fcadd9c04f9c3af0776bce1
Randon Studer <89085444+Rosidvas@users.noreply.github.com>
2024-02-20 06:52:39 +0100

    Add files via upload

--- main2.py
+++ main2.py
@@ -4,12 +4,14 @@ import pyautogui
 from functools import partial
 from pathlib import Path
 
+global x 
 x = 1400
 cycle = 0
 check = 1
 idle_num = [1, 2, 3, 4, 5, 8]
 walk_left = [ 6, 7]
 walk_right = [9, 10]
+teleport = [11]
 event_number = random.randrange(1, 3, 1)
 impath = './Animations/'
 
@@ -28,24 +30,27 @@ def gif_work(cycle, frames, event_number, first_num, last_num):
 def update(cycle, check, event_number, x, x_left_edge, x_right_edge):
     global frame
     y = (screen_height - window.winfo_reqheight())
-    print(x)
 
     if check == 0:
         frame = idle_frames[cycle]
         cycle, event_number = gif_work(cycle, idle_frames, event_number, 1, 3)
-    elif check == 1 and x > 0:
+    elif check == 1 and x > -100:
         frame = walk_left_frames[cycle]
         cycle, event_number = gif_work(cycle, walk_left_frames, event_number, 1, 3)
         x -= 50
-    elif check == 2 and x <  window.winfo_screenwidth() -200:
+    elif check == 2 and x < screen_width - window.winfo_reqwidth() - 200:
         frame = walk_right_frames[cycle]
         cycle, event_number = gif_work(cycle, walk_right_frames, event_number, 1, 3)
-        x += 50 
+        x += 50
+    elif check == 11:
+        frame = walk_right_frames[cycle]
+        cycle, event_number = gif_work(cycle, walk_right_frames, event_number, 1, 3)
+        x += 100
     else:
         event_number = random.randrange(1, 11, 1)
         print("skipped")
 
-    
+    print(x)
     window.geometry(f'+{x}+{y}')
     label.configure(image=frame)
     window.after(1,event,cycle,check,event_number,x, x_left_edge, x_right_edge)
@@ -53,6 +58,7 @@ def update(cycle, check, event_number, x, x_left_edge, x_right_edge):
 
 def event(cycle, check, event_number, x, x_left_edge, x_right_edge):
     
+
     if event_number in idle_num:
         check = 0
         print('idle')
@@ -65,28 +71,44 @@ def event(cycle, check, event_number, x, x_left_edge, x_right_edge):
         check = 2
         print('walking towards right')
         window.after(200, update, cycle, check, event_number, x, x_left_edge, x_right_edge)  # no 8,9 = walk towards right
+    elif event_number in teleport:
+        check = 11
+        print('teleport')
+        window.after(100, update, cycle, check, event_number, x, x_left_edge, x_right_edge)
 
 
 window = tk.Tk()
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
+
 # Start Window position
 x_coordinate = (screen_width - window.winfo_reqwidth()) // 2
 y_coordinate = (screen_height - window.winfo_reqheight()) // 2
-window.geometry(f'+{x_coordinate}+{y_coordinate}')
-print( window.winfo_screenwidth())
+window.geometry(f'{window.winfo_reqwidth()}x{window.winfo_reqheight()}+{x_coordinate}+{y_coordinate}')
+print(window.winfo_screenwidth())
 print("starting")
+
+
 
 # Calculate the x-coordinate for screen edges
 x_left_edge = (screen_width - window.winfo_reqwidth()) // 2
 x_right_edge = x_left_edge + screen_width
 
+
 def onclick_teleport(event):
-    event_number = 9
+    global x
+    x = window.winfo_x()
+    # event_number = 11
+    print("pressed")
     if event.widget == window:
         print("fake")
-    update(cycle, check, event_number, x, x_left_edge, x_right_edge)
+
+    x += 200
+    window.geometry(f'+{x}+{(screen_height - window.winfo_reqheight())}')
+    # window.after(1, update, cycle, check, event_number, x, x_left_edge, x_right_edge)
+
+
     
         
 window.bind("<Button-1>", onclick_teleport)    
